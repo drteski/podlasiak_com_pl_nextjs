8031328beaefe1969dac76b12b91635d7494c4ea
Krzysztof Tomaszewski <drteski@gmail.com>
2023-12-20 11:24:48 +0100

    init

--- app/[locale]/components/FooterContact.jsx
+++ app/[locale]/components/FooterContact.jsx
@@ -15,27 +15,30 @@ const FooterContact = ({ contact, locale }) => {
           : currentContact.title[`${locale}`]}
       </h3>
       <div className="">
-        {currentContact.contactPlaces.map((details) => (
-          <div key={details._key}>
-            <div className="flex items-center gap-2 text-xl font-medium text-main-fr">
-              <SanityImage data={details.icon} className="invert" />
-              <h4>{details.title}</h4>
-            </div>
-            <div>
-              {details.contacts.map((detail) => (
-                <div className="mb-8 ml-10 flex flex-col" key={detail._key}>
-                  <h5 className="py-2 font-medium">{detail.subTitle}</h5>
-                  {detail.contactTypes.map((type) => (
-                    <span key={type._key}>
-                      <strong>{type.name}: </strong>
-                      {type.value}
-                    </span>
+        {!currentContact.contactPlaces &&
+          currentContact.contactPlaces.map((details) => (
+            <div key={details._key}>
+              <div className="flex items-center gap-2 text-xl font-medium text-main-fr">
+                <SanityImage data={details.icon} className="invert" />
+                <h4>{details.title}</h4>
+              </div>
+              <div>
+                {!details.contacts &&
+                  details.contacts.map((detail) => (
+                    <div className="mb-8 ml-10 flex flex-col" key={detail._key}>
+                      <h5 className="py-2 font-medium">{detail.subTitle}</h5>
+                      {!detail.contactTypes &&
+                        detail.contactTypes.map((type) => (
+                          <span key={type._key}>
+                            <strong>{type.name}: </strong>
+                            {type.value}
+                          </span>
+                        ))}
+                    </div>
                   ))}
-                </div>
-              ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
