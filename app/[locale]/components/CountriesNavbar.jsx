a81119a011607057e6e7e907562e50f7d1ea1ee8
Krzysztof Tomaszewski <drteski@gmail.com>
2023-12-27 14:03:40 +0100

    init

--- app/[locale]/components/CountriesNavbar.jsx
+++ app/[locale]/components/CountriesNavbar.jsx
@@ -12,7 +12,7 @@ const CountriesNavbar = forwardRef(({ countriesData }, ref) => {
     <>
       <div
         ref={ref}
-        className={`flex flex-wrap h-fit items-center justify-center gap-4 bg-main-fr/40 py-6 bottom-dynamic-1/2 mx-dynamic-1/2`}
+        className={`flex flex-wrap h-fit items-center justify-center gap-[10px] bg-main-fr/40 py-8 bottom-dynamic-1/2 mx-dynamic-1/2`}
       >
         {countries().map((country) => {
           return (
@@ -22,7 +22,7 @@ const CountriesNavbar = forwardRef(({ countriesData }, ref) => {
               href={country.link}
             >
               <SanityImage
-                className="w-8 lg:w-10 transition group-hover/flag:scale-125 rounded-sm border border-main-bg/40"
+                className="w-8 lg:w-[52px] transition group-hover/flag:scale-125 rounded-sm border border-main-bg/40"
                 data={country.flag.asset}
               />
               <div className="bg-main-fr border text-main-bg pointer-events-none absolute -top-14 right-1/2 translate-x-1/2 whitespace-nowrap rounded-sm px-3.5 py-1.5 text-xs opacity-0 transition group-hover/flag:opacity-100">
